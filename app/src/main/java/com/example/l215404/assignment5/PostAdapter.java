b6d519f013b007bb6cd15a0651fd47586b34452f
Urwa Ihtesham <urwaihtesham@gmail.com>
2024-12-12 23:28:32 +0500

    mainactivity: created post data model

---
+++ app/src/main/java/com/example/l215404/assignment5/PostAdapter.java
@@ -0,0 +1,9 @@
+package com.example.l215404.assignment5;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder> {
+    private List<Post> postList;
+    private onPostAction
+
+}
